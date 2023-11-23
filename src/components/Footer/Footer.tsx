23cb81aa9cb543478836d264e8f6842e899ee7ee
pizarrosch <bayenrshc89@gmail.com>
2023-11-23 23:03:02 +0100

    Set up links to the social media icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,15 +5,26 @@ import gitHub from '../../../public/assets/github-mark.svg';
 import X from '../../../public/assets/x-social-media-logo-icon.svg';
 import facebook from '../../../public/assets/2021_Facebook_icon.svg.png';
 import instagram from '../../../public/assets/Instagram_logo_2022.svg.png';
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <div className={s.root}>
-            <Image className={s.image} src={gitHub} alt='' />
-            <Image className={s.image} src={X} alt='' />
-            <Image className={s.image} src={linkedIn} alt='' />
-            <Image className={s.image} src={facebook} alt='' />
-            <Image className={s.image} src={instagram} alt='' />
+            <Link href='https://github.com/pizarrosch' title='github' target='_blank'>
+                <Image className={s.image} src={gitHub} alt='' />
+            </Link>
+            <Link href='https://twitter.com/zshomakhov' title='X(former Twitter)' target='_blank'>
+                <Image className={s.image} src={X} alt='' />
+            </Link>
+            <Link href='https://www.linkedin.com/in/zaur-shomakhov-13a106122/' title='linkedIn' target='_blank'>
+                <Image className={s.image} src={linkedIn} alt='' />
+            </Link>
+            <Link href='https://www.facebook.com/zaur.shomakhov' title='facebook' target='_blank'>
+                <Image className={s.image} src={facebook} alt='' />
+            </Link>
+            <Link href='https://www.instagram.com/the_big_chinese/' title='instagram' target='_blank'>
+                <Image className={s.image} src={instagram} alt='' />
+            </Link>
         </div>
     )
 }
