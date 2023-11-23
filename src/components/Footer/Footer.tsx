3bcda32e6cec2b3aeaecc1ab681e9297ec870a68
pizarrosch <bayenrshc89@gmail.com>
2023-11-23 22:45:58 +0100

    Place social media symbols in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,19 @@
 import s from './Footer.module.scss';
+import Image from "next/image";
+import linkedIn from '../../../public/assets/linkedin.svg';
+import gitHub from '../../../public/assets/github-mark.svg';
+import X from '../../../public/assets/x-social-media-logo-icon.svg';
+import facebook from '../../../public/assets/2021_Facebook_icon.svg.png';
+import instagram from '../../../public/assets/Instagram_logo_2022.svg.png';
 
 export default function Footer() {
     return (
-        <div className={s.root}></div>
+        <div className={s.root}>
+            <Image className={s.image} src={gitHub} alt='' />
+            <Image className={s.image} src={X} alt='' />
+            <Image className={s.image} src={linkedIn} alt='' />
+            <Image className={s.image} src={facebook} alt='' />
+            <Image className={s.image} src={instagram} alt='' />
+        </div>
     )
 }
