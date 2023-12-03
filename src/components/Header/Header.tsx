2fd6c1006d2e6edbe67b8b9979498300f3fa8d01
pizarrosch <bayenrshc89@gmail.com>
2023-12-03 16:16:54 +0100

    Create a dummy paragraph for the main page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import s from './Header.module.scss';
+import Image from "next/image";
+import myPic from '../../../public/assets/Zaur.jpeg';
 
 export default function Header() {
     return (
-        <div className={s.root}></div>
+        <div className={s.root}>
+            <Image src={myPic} alt='' className={s.image}/>
+            <span className={s.name}>Zaur Shomakhov</span>
+        </div>
     )
 }
