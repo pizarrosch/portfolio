b77805dd1bb45c4773e3d4bc81298a1040638c6a
pizarrosch <bayenrshc89@gmail.com>
2023-12-04 23:57:20 +0100

    Add a self-description text on the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,6 +8,17 @@ export default function About() {
         <Layout>
             <div className={s.root}>
                 <Image src={Zaur} alt='' className={s.profileImage}/>
+                <article>
+                    <p>
+                        My name is Zaur. I am a chemist who one day wanted <br/> to change his profession to that of a frontend developer <br/> and realized one day he will stay a programmer for life.
+                    </p>
+                    <p>
+                        After having successfully finished a 14 months online course, <br/> including a lot of projects and assisted by experienced mentors, <br/> I am ready to step into the professional life and start to develop <br/> my skills and contribute to the success of a new family I will work with.
+                    </p>
+                    <p>
+                        Feel free to click on the Projects button on the navigation menu to look <br/> into some of my projects.
+                    </p>
+                </article>
             </div>
         </Layout>
     )
