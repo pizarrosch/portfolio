a202d86d558baebab942917717deebdbd5cfd88e
pizarrosch <bayenrshc89@gmail.com>
2023-12-08 00:55:03 +0100

    Add language skills

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,16 +14,20 @@ export default function About() {
                     <span>About me</span>
                     <article>
                         <p>
-                            My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to that of a
+                            My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to that of
+                            a
                             frontend developer and realized one <br/> day he will stay a programmer for life.
                         </p>
                         <p>
-                            After having successfully finished a 14 months online course, <br/> including a lot of projects
+                            After having successfully finished a 14 months online course, <br/> including a lot of
+                            projects
                             and assisted by experienced mentors, <br/> I am ready to step into the professional life and
-                            start to develop <br/> my skills and contribute to the success of a new family I will work <br/> with.
+                            start to develop <br/> my skills and contribute to the success of a new family I will
+                            work <br/> with.
                         </p>
                         <p>
-                            Feel free to click on the Projects button on the navigation menu to <br/> look into some of my
+                            Feel free to click on the Projects button on the navigation menu to <br/> look into some of
+                            my
                             projects.
                         </p>
                     </article>
@@ -36,6 +40,13 @@ export default function About() {
                                 <Skills skill={skill}/>
                             )
                         })}
+                    </div>
+                    <span>My languages:</span>
+                    <div className={s.langSkillsContainer}>
+                        <Skills skill='English - fluent'/>
+                        <Skills skill='German - fluent'/>
+                        <Skills skill='Spanish - basics'/>
+                        <Skills skill='Russian - mother tongue'/>
                     </div>
                 </div>
             </div>
