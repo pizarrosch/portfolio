0bc4b4cdf4edb15f25fe179c6ae750c52c8e07d0
pizarrosch <bayenrshc89@gmail.com>
2023-12-08 23:37:24 +0100

    Modify the about page, change the background-color of the main page, as well as that of header and footer

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,44 +9,52 @@ export default function About() {
     return (
         <Layout>
             <div className={s.root}>
-                <Image src={Zaur} alt='' className={s.profileImage}/>
-                <div className={s.aboutMeWrapper}>
-                    <span>About me</span>
-                    <article>
-                        <p>
-                            My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to that of
-                            a
-                            frontend developer and realized one <br/> day he will stay a programmer for life.
-                        </p>
-                        <p>
-                            After having successfully finished a 14 months online course, <br/> including a lot of
-                            projects
-                            and assisted by experienced mentors, <br/> I am ready to step into the professional life and
-                            start to develop <br/> my skills and contribute to the success of a new family I will
-                            work <br/> with.
-                        </p>
-                        <p>
-                            Feel free to click on the Projects button on the navigation menu to <br/> look into some of
-                            my
-                            projects.
-                        </p>
-                    </article>
+                <div className={s.aboutMeContainer}>
+                    <Image src={Zaur} alt='' className={s.profileImage}/>
+                    <div className={s.aboutMeWrapper}>
+                        <span className={s.title}>About me</span>
+                        <article>
+                            <p>
+                                My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to
+                                that of a frontend developer and realized one <br/> day he will stay a programmer for
+                                life.
+                            </p>
+                            <p>
+                                After having successfully finished a 14 months online course, <br/> including a lot of
+                                projects
+                                and assisted by experienced mentors, <br/> I am ready to step into the professional life
+                                and
+                                start to develop <br/> my skills and contribute to the success of a new family I will
+                                work <br/> with.
+                            </p>
+                            <p>
+                                Feel free to click on the Projects button on the navigation menu to <br/> look into some
+                                of
+                                my
+                                projects.
+                            </p>
+                        </article>
+                    </div>
                 </div>
                 <div className={s.skillsWrapper}>
-                    <span>My skills:</span>
-                    <div className={s.skillsContainer}>
-                        {SKILLS.map((skill: string) => {
-                            return (
-                                <Skills skill={skill}/>
-                            )
-                        })}
+                    <div className={s.wrapper}>
+                        <span className={s.title}>My skills:</span>
+                        <div className={s.skillsContainer}>
+                            {SKILLS.map((skill: string) => {
+                                return (
+                                    <Skills skill={skill}/>
+                                )
+                            })}
+                        </div>
                     </div>
-                    <span>My languages:</span>
-                    <div className={s.langSkillsContainer}>
-                        <Skills skill='English - fluent'/>
-                        <Skills skill='German - fluent'/>
-                        <Skills skill='Spanish - basics'/>
-                        <Skills skill='Russian - mother tongue'/>
+                    <div className={s.wrapper}>
+                        <span className={s.title}>My languages:</span>
+                        <div className={s.langSkillsContainer}>
+                            <Skills skill='English - fluent'/>
+                            <Skills skill='German - fluent'/>
+                            <Skills skill='Spanish - basics'/>
+                            <Skills skill='Russian - mother tongue'/>
+                        </div>
                     </div>
                 </div>
             </div>
