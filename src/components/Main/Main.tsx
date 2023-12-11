6ff5d8bf978037765e33b88a87ce8f16aa3328e9
pizarrosch <bayenrshc89@gmail.com>
2023-12-11 21:29:59 +0100

    Finish the switcher functionality

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,28 +1,20 @@
 import s from './Main.module.scss';
 import ParticlesComponent from "@/components/Particles/Particles";
-import {activate} from "@/redux/slices/activateAnimationSlice";
-import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
-import {RootState} from "@/redux/store";
+
 export default function Main() {
 
-  const activationStatus = useAppSelector((state: RootState) => state.activationsStatus);
-  const dispatch = useAppDispatch();
-
-    // @ts-ignore
-    return (
-        <div className={s.root}>
-          <div onClick={() => {
-            return activationStatus.active ? dispatch(activate(false)) : dispatch(activate(true));
-          }}>{activationStatus.active ? 'Turn off animation' : 'Turn on animation'}</div>
-            <div style={{position: "absolute"}} className={s.background}>
-                <ParticlesComponent/>
-            </div>
-            <div className={s.greetingsTitle}>Hi! My name is Zaur Shomakhov</div>
-            <p className={s.greetingsParagraph}>
-                I am a new born frontend developer, who wants to put the whole energy into new profession.<br/>
-                If you need someone with the good knowledge of React, Next.js and Typescript, I am glad to
-                start right away!
-            </p>
-        </div>
-    )
+  // @ts-ignore
+  return (
+    <div className={s.root}>
+      <div style={{position: "absolute"}} className={s.background}>
+        <ParticlesComponent/>
+      </div>
+      <div className={s.greetingsTitle}>Hi! My name is Zaur Shomakhov</div>
+      <p className={s.greetingsParagraph}>
+        I am a new born frontend developer, who wants to put the whole energy into new profession.<br/>
+        If you need someone with the good knowledge of React, Next.js and Typescript, I am glad to
+        start right away!
+      </p>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,28 @@ import {Inter} from 'next/font/google'
 import Main from "@/components/Main/Main";
 import Layout from "@/Layout/Layout";
 import s from "@/pages/styles/index.module.scss";
+import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
+import {RootState} from "@/redux/store";
+import {activate} from "@/redux/slices/activateAnimationSlice";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
+  const activationStatus = useAppSelector((state: RootState) => state.activationsStatus);
+  const dispatch = useAppDispatch();
+
   return (
     <Layout>
       <div className={s.switcherContainer}>
-        <span>Animation ON</span>
-        <div className={s.switchCircleWrapper}>
-          <div></div>
+        <span style={{color: '#5d5d5d'}}>Animation</span>
+        <div
+          className={activationStatus.active ? s.switchCircleWrapperOn : s.switchCircleWrapperOff}
+          onClick={() => {
+            return activationStatus.active ? dispatch(activate(false)) : dispatch(activate(true));
+          }
+          }
+        >
+          <div className={activationStatus.active ? s.switcherOn : s.switcherOff}></div>
         </div>
       </div>
       <Main/>
