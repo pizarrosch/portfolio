038570013a8dae602af1b5fea28f40a5b5a0cbfd
pizarrosch <bayenrshc89@gmail.com>
2024-01-04 21:53:31 +0100

    Implemented dark theme support in project pages and fixed horizontal scrolling issue.
    Updated project pages to support dark mode and made some style changes for better UI experience in both light and dark modes. The width of the page has been changed from 100vw to 100% to eliminate the unnecessary horizontal scrollbar occurring due to viewport width discrepancy. Matching theme colors have been set for various elements in dark mode for a consistent look and feel.

--- src/components/ProjectItem/ProjectItem.tsx
+++ src/components/ProjectItem/ProjectItem.tsx
@@ -2,19 +2,24 @@ import Image from "next/image";
 import {TProject} from "@/types";
 import s from './ProjectItem.module.scss';
 import Link from "next/link";
+import {useAppSelector} from "@/hooks/hooks";
+import {RootState} from "@/redux/store";
 
 export default function ProjectItem({src, title, description, url}: TProject) {
+
+  const actualTheme = useAppSelector((state: RootState) => state.theme);
+
   return (
     <div className={s.root}>
-      <div className={s.imageWrapper}>
+      <div className={actualTheme.theme === 'light' ? s.imageWrapper : s.imageWrapperDark}>
         <Image src={src} alt='' className={s.pjImage} width={600}/>
       </div>
       <div>
-        <h1>{title}</h1>
-        <p className={s.description}>{description}</p>
+        <h1 className={actualTheme.theme === 'light' ? s.title : s.titleDark}>{title}</h1>
+        <p className={actualTheme.theme === 'light' ? s.description : s.descriptionDark}>{description}</p>
       </div>
       <Link href={url ? url : ''} target={url ? "_blank" : ''} className={s.buttonLink}>
-        <button className={s.button}>Check it out</button>
+        <button className={actualTheme.theme === 'light' ? s.button : s.buttonDark}>Check it out</button>
       </Link>
     </div>
   )
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,18 +1,17 @@
 import Layout from "@/Layout/Layout";
 import s from './styles/projects.module.scss';
-import bookStorePj from '../../public/assets/bookStoreNextJsPj.png';
 import ProjectItem from "@/components/ProjectItem/ProjectItem";
-import kanbanPjImg from '../../public/assets/kanban-board.png';
-import weatherWidgetPjImg from '../../public/assets/weather-widget.png';
-import todoListImg from '../../public/assets/todo-project.png';
 import leftArrow from '../../public/assets/arrow-left-3099.svg';
 import Image from "next/image";
 import {pjItemsData} from "@/data";
 import React, {useState} from "react";
+import {useAppSelector} from "@/hooks/hooks";
+import {RootState} from "@/redux/store";
 
 export default function Projects() {
 
   const [slideIndex, setSlideIndex] = useState(0);
+  const actualTheme = useAppSelector((state: RootState) => state.theme);
 
   function onRightClick() {
     if (slideIndex === pjItemsData.length - 1) return;
@@ -26,7 +25,7 @@ export default function Projects() {
 
   return (
     <Layout>
-      <div className={s.root}>
+      <div className={actualTheme.theme === 'light' ? s.root : s.darkRoot}>
         <Image src={leftArrow} alt='' width={64} height={64} className={slideIndex > 0 ? s.arrowLeftActive : s.arrowLeftInactive} onClick={onLeftClick}/>
         <ProjectItem src={pjItemsData[slideIndex].src} title={pjItemsData[slideIndex].title}
                      description={pjItemsData[slideIndex].description} url={pjItemsData[slideIndex].url}/>
