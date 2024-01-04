d0dc77ac0d5420652d40a8d8e8e4d12251107069
pizarrosch <bayenrshc89@gmail.com>
2024-01-04 03:13:13 +0100

    Wrap the icons and the backdrop div to maintain the same position of the backdrop on all the pages

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,25 +15,33 @@ export default function Footer() {
 
   return (
     <div className={actualTheme.theme === 'light' ? s.root : s.darkRoot}>
-      <div className={s.imageBackdropGithub}></div>
-      <Link href='https://github.com/pizarrosch' title='github' target='_blank'>
-          <Image className={s.image} src={gitHub} alt='' />
-      </Link>
+      <div className={s.githubIconWrapper}>
+        <div className={s.imageBackdropGithub}></div>
+        <Link href='https://github.com/pizarrosch' title='github' target='_blank'>
+          <Image className={s.image} src={gitHub} alt=''/>
+        </Link>
+      </div>
       <Link href='https://twitter.com/zshomakhov' title='X(former Twitter)' target='_blank'>
         <Image className={s.x_ixon} src={X} alt=''/>
       </Link>
-      <div className={s.imageBackdropLinkedIn}></div>
-      <Link href='https://www.linkedin.com/in/zaur-shomakhov-13a106122/' title='linkedIn' target='_blank'>
-        <Image className={s.image} src={linkedIn} alt=''/>
-      </Link>
-      <div className={s.imageBackdropFacebook}></div>
-      <Link href='https://www.facebook.com/zaur.shomakhov' title='facebook' target='_blank'>
-        <Image className={s.image} src={facebook} alt=''/>
-      </Link>
-      <div className={s.imageBackdropInstagram}></div>
-      <Link href='https://www.instagram.com/the_big_chinese/' title='instagram' target='_blank'>
-        <Image className={s.image} src={instagram} alt=''/>
-      </Link>
+      <div className={s.linkedInIconWrapper}>
+        <div className={s.imageBackdropLinkedIn}></div>
+        <Link href='https://www.linkedin.com/in/zaur-shomakhov-13a106122/' title='linkedIn' target='_blank'>
+          <Image className={s.image} src={linkedIn} alt=''/>
+        </Link>
+      </div>
+      <div className={s.facebookIconWrapper}>
+        <div className={s.imageBackdropFacebook}></div>
+        <Link href='https://www.facebook.com/zaur.shomakhov' title='facebook' target='_blank'>
+          <Image className={s.image} src={facebook} alt=''/>
+        </Link>
+      </div>
+      <div className={s.instagramIconWrapper}>
+        <div className={s.imageBackdropInstagram}></div>
+        <Link href='https://www.instagram.com/the_big_chinese/' title='instagram' target='_blank'>
+          <Image className={s.image} src={instagram} alt=''/>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,60 +4,65 @@ import Zaur from '../../public/assets/Zaur.jpeg';
 import Image from "next/image";
 import Skills from "@/components/Skills/Skills";
 import {SKILLS} from "@/data";
+import {useAppSelector} from "@/hooks/hooks";
+import {RootState} from "@/redux/store";
 
 export default function About() {
-    return (
-        <Layout>
-            <div className={s.root}>
-                <div className={s.aboutMeContainer}>
-                    <Image src={Zaur} alt='' className={s.profileImage}/>
-                    <div className={s.aboutMeWrapper}>
-                        <span className={s.title}>About me</span>
-                        <article>
-                            <p>
-                                My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to
-                                that of a frontend developer and realized one <br/> day he will stay a programmer for
-                                life.
-                            </p>
-                            <p>
-                                After having successfully finished a 14 months online course, <br/> including a lot of
-                                projects
-                                and assisted by experienced mentors, <br/> I am ready to step into the professional life
-                                and
-                                start to develop <br/> my skills and contribute to the success of a new family I will
-                                work <br/> with.
-                            </p>
-                            <p>
-                                Feel free to click on the Projects button on the navigation menu to <br/> look into some
-                                of
-                                my
-                                projects.
-                            </p>
-                        </article>
-                    </div>
-                </div>
-                <div className={s.skillsWrapper}>
-                    <div className={s.wrapper}>
-                        <span className={s.title}>My skills:</span>
-                        <div className={s.skillsContainer}>
-                            {SKILLS.map((skill: string, id: number) => {
-                                return (
-                                    <Skills skill={skill} key={id}/>
-                                )
-                            })}
-                        </div>
-                    </div>
-                    <div className={s.wrapper}>
-                        <span className={s.title}>My languages:</span>
-                        <div className={s.langSkillsContainer}>
-                            <Skills skill='English - fluent'/>
-                            <Skills skill='German - fluent'/>
-                            <Skills skill='Spanish - basics'/>
-                            <Skills skill='Russian - mother tongue'/>
-                        </div>
-                    </div>
-                </div>
+
+  const actualTheme = useAppSelector((state: RootState) => state.theme);
+
+  return (
+    <Layout>
+      <div className={actualTheme.theme === 'light' ? s.root : s.darkRoot}>
+        <div className={s.aboutMeContainer}>
+          <Image src={Zaur} alt='' className={s.profileImage}/>
+          <div className={s.aboutMeWrapper}>
+            <span className={s.title}>About me</span>
+            <article>
+              <p>
+                My name is Zaur. I am a chemist who one day wanted to change <br/> his profession to
+                that of a frontend developer and realized one <br/> day he will stay a programmer for
+                life.
+              </p>
+              <p>
+                After having successfully finished a 14 months online course, <br/> including a lot of
+                projects
+                and assisted by experienced mentors, <br/> I am ready to step into the professional life
+                and
+                start to develop <br/> my skills and contribute to the success of a new family I will
+                work <br/> with.
+              </p>
+              <p>
+                Feel free to click on the Projects button on the navigation menu to <br/> look into some
+                of
+                my
+                projects.
+              </p>
+            </article>
+          </div>
+        </div>
+        <div className={s.skillsWrapper}>
+          <div className={s.wrapper}>
+            <span className={s.title}>My skills:</span>
+            <div className={s.skillsContainer}>
+              {SKILLS.map((skill: string, id: number) => {
+                return (
+                  <Skills skill={skill} key={id}/>
+                )
+              })}
             </div>
-        </Layout>
-    )
+          </div>
+          <div className={s.wrapper}>
+            <span className={s.title}>My languages:</span>
+            <div className={s.langSkillsContainer}>
+              <Skills skill='English - fluent'/>
+              <Skills skill='German - fluent'/>
+              <Skills skill='Spanish - basics'/>
+              <Skills skill='Russian - mother tongue'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
