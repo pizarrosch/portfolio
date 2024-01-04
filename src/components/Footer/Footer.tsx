515ec299387ab3009e33edc49f9cca65e45c0bc0
pizarrosch <bayenrshc89@gmail.com>
2024-01-04 01:38:10 +0100

    Add white backdrop for icons except for X icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,18 +15,22 @@ export default function Footer() {
 
   return (
     <div className={actualTheme.theme === 'light' ? s.root : s.darkRoot}>
+      <div className={s.imageBackdropGithub}></div>
       <Link href='https://github.com/pizarrosch' title='github' target='_blank'>
-        <Image className={s.image} src={gitHub} alt=''/>
+          <Image className={s.image} src={gitHub} alt='' />
       </Link>
       <Link href='https://twitter.com/zshomakhov' title='X(former Twitter)' target='_blank'>
         <Image className={s.x_ixon} src={X} alt=''/>
       </Link>
+      <div className={s.imageBackdropLinkedIn}></div>
       <Link href='https://www.linkedin.com/in/zaur-shomakhov-13a106122/' title='linkedIn' target='_blank'>
         <Image className={s.image} src={linkedIn} alt=''/>
       </Link>
+      <div className={s.imageBackdropFacebook}></div>
       <Link href='https://www.facebook.com/zaur.shomakhov' title='facebook' target='_blank'>
         <Image className={s.image} src={facebook} alt=''/>
       </Link>
+      <div className={s.imageBackdropInstagram}></div>
       <Link href='https://www.instagram.com/the_big_chinese/' title='instagram' target='_blank'>
         <Image className={s.image} src={instagram} alt=''/>
       </Link>
