fdf93a84e501209fc81c23655521b029e96cf28e
pizarrosch <bayenrshc89@gmail.com>
2024-01-21 21:53:12 +0100

    Substitute the switcher with the template from art design, change the main page text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
 import {RootState} from "@/redux/store";
 import {activate} from "@/redux/slices/activateAnimationSlice";
 
+const {Switch, ConfigProvider} = require('antd');
+import React from "react";
+
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
@@ -15,31 +18,42 @@ export default function Home() {
 
   return (
     <Layout>
-      <div className={s.switcherContainer}>
+      <>
+        <div className={s.switcherContainer}>
         <span className={
           actualTheme.theme === 'light' ? s.titleLight : s.titleDark
-        }>Animation</span>
-        <div
-          className={
-            activationStatus.active && actualTheme.theme === 'light' ? s.switchCircleWrapperOn :
-              activationStatus.active && actualTheme.theme === 'dark' ? s.switchCircleWrapperOnDarkMode :
-                activationStatus.active && actualTheme.theme === 'darkHelloween' ? s.switchCircleWrapperOnHelloweenMode :
-                  s.switchCircleWrapperOff
-          }
-          onClick={() => {
-            return activationStatus.active ? dispatch(activate(false)) : dispatch(activate(true));
-          }
-          }
-        >
-          <div className={
-            activationStatus.active && actualTheme.theme === 'light' ? s.switcherOn :
-              activationStatus.active && actualTheme.theme === 'dark' ? s.switcherOnDarkMode :
-                activationStatus.active && actualTheme.theme === 'darkHelloween' ? s.switcherOnHelloweenMode :
-                  s.switcherOff
-          }></div>
+        }>
+          Animation
+        </span>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary:
+                  actualTheme.theme === 'dark' ? '#F6F7F9' :
+                    actualTheme.theme === 'light' ? '#BD6BBD' :
+                      '#FF9980'
+              },
+              components: {
+                Switch: {
+                  handleBg:
+                    actualTheme.theme === 'dark' && activationStatus.active ? '#383E47' :
+                      actualTheme.theme === 'dark' && !activationStatus.active ? '#F6F7F9' :
+                        actualTheme.theme === 'darkHelloween' && activationStatus.active ? '#383E47' :
+                          actualTheme.theme === 'light' ? '#F6F7F9' : '#FF9980'
+                }
+              }
+            }}
+          >
+            <Switch checked={activationStatus.active} onClick={
+              () => {
+                return activationStatus.active ? dispatch(activate(false)) : dispatch(activate(true));
+              }
+            }/>
+          </ConfigProvider>
         </div>
-      </div>
-      <Main/>
+        <Main/>
+      </>
+
     </Layout>
   )
 }
